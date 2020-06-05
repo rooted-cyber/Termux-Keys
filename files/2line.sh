3a9bcdbb0601b70337cfa8ac53bbd9229e781d5c
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2020-06-05 12:10:59 +0530

    Add files via upload

--- files/2line.sh
+++ files/2line.sh
@@ -12,7 +12,13 @@ banner() {
 		cd ~/.termux
 		rm -f termux.properties > /dev/null 2>&1
 		echo "extra-keys = [['/','ls','$','~','UP','exit','*'],['ex','CTRL','ENTER','LEFT','DOWN','RIGHT','F2']]" >> $HOME/.termux/termux.properties
+		printf "\n# Open a new terminal with ctrl + t (volume down + t)\nshortcut.create-session = ctrl + t\n# Go one session down with (for example) ctrl + 2\nshortcut.next-session = ctrl + 2\n# Go one session up with (for example) ctrl + 1\nshortcut.previous-session = ctrl + 1\n# Rename a session with (for example) ctrl + n\nshortcut.rename-session = ctrl + n" >> ~/.termux/termux.properties
 		random
+		printf " \n Added shortcut -- ctrl +t to open new session\n"
+		printf "ctrl +1 -- next session\n"
+		printf "ctrl +2 -- previos session\n"
+		printf "ctrl +n -- rename session\n"
+		printf "Added this command for exit all sessikn : ex\n"
 		printf "\n Restart Termux\n"
 		}
 		menu
